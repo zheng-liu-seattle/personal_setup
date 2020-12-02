5463a53bb847d64fee8bb7f0e5e265cb6ce66b67
Zheng Liu <zheng.liu@grab.com>
2020-12-02 11:51:44 -0800

    add update dns go daddy script

---
+++ update_dns.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mydomain="Youe Domain"
+myhostname="Your Host Name in A Record"
+gdapikey="secret:key"
+logdest="local7.info"
+
+myip=`curl -s "https://api.ipify.org"`
+dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
+gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
+echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
+
+if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
+  echo "IP has changed!! Updating on GoDaddy"
+  curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
+  logger -p $logdest "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}"
+fi
